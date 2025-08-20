d240b8a5423e196dac626c421fa8811d8e9ffd7b
kevinhh12 <117544888+kevinhh12@users.noreply.github.com.>
2025-08-19 21:19:39 -0600

    switch to supabase as db

--- backend/src/db.js
+++ backend/src/db.js
@@ -4,10 +4,11 @@ import pkg from 'pg';
 const { Pool } = pkg;
 
 export const db = new Pool({
+  connectionString: process.env.DATABASE_URL,
   host: process.env.DB_HOST,
   port: process.env.DB_PORT,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
   ssl: { rejectUnauthorized: false }
-});
+});s
