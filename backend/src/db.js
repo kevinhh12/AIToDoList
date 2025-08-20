8f65a3e4f0972c630a34ad2989887681f1d56232
kevinhh12 <117544888+kevinhh12@users.noreply.github.com.>
2025-08-19 21:24:57 -0600

    bug dixed again

--- backend/src/db.js
+++ backend/src/db.js
@@ -5,7 +5,7 @@ const { Pool } = pkg;
 
 export const db = new Pool({
   connectionString: process.env.DATABASE_URL,
-  host: processs.env.DB_HOST,
+  host: process.env.DB_HOST,
   port: process.env.DB_PORT,
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
